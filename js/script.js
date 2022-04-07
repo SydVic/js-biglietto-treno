eca1f7ad99e3542c1d6bc4230419e4e5fde4a690
SydVic <fabrizio.bocca27@gmail.com>
2022-04-07 14:11:16 +0200

    pseudocodice js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// raccolta input utente (km da percorrere e età del passeggero)
+const userDistance = prompt("Quanti km devi percorrere?");
+console.log(userDistance, typeof(userDistance));
+
+// calcolo del prezzo al lordo dello sconto (km da pecorrere * 0,21 al km)
+
+// determinazione della percentuale di sconto (SE età < 18 = 20% - SE età > 65 = 40% - ALTRIMENTI niente sconto )
+
+// calcolo del prezzo scontato
+
+// stampa in HTML del prezzo finale
+
