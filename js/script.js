3f8cb66397bac085744849bd0460647cc0821839
SydVic <fabrizio.bocca27@gmail.com>
2022-04-07 14:42:57 +0200

    creato script calcolo prezzo biglietto

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 // raccolta input utente (km da percorrere e età del passeggero)
-const userDistance = prompt("Quanti km devi percorrere?");
+const userDistance = parseInt(prompt("Quanti km devi percorrere?"));
 console.log(userDistance, typeof(userDistance));
 
+const userAge = parseInt(prompt("Qual'è la tua età?"));
+console.log(userAge, typeof(userAge));
+
 // calcolo del prezzo al lordo dello sconto (km da pecorrere * 0,21 al km)
+const pricePerKm = 0.21;
+
+const fullTicketPrice = userDistance * pricePerKm;
+console.log(fullTicketPrice, typeof(fullTicketPrice));
 
 // determinazione della percentuale di sconto (SE età < 18 = 20% - SE età > 65 = 40% - ALTRIMENTI niente sconto )
+// e calcolo dello sconto
+let finalTicketPrice;
 
-// calcolo del prezzo scontato
+if ( userAge < 18 ) {
+    finalTicketPrice = fullTicketPrice * 0.8;
+} else if (userAge > 65) {
+    finalTicketPrice = fullTicketPrice * 0.6;
+} else {
+    finalTicketPrice = fullTicketPrice;
+}
+
+const finalPriceRounded = finalTicketPrice.toFixed(2);
 
 // stampa in HTML del prezzo finale
-
+const outputFinalPrice= `Il prezzo del tuo biglietto è: ${finalPriceRounded} \u20ac`;
+console.log(finalTicketPrice, typeof(finalTicketPrice));
+document.getElementById('final-price').innerHTML = outputFinalPrice;
